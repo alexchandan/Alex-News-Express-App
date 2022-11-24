8fdd3602f73ea66073beb4144a758b5492b8e42b
Alex Chandan <chandanakela321@gmail.com>
2022-11-24 14:54:03 +0530

    Next and Previous Btn added

--- src/Components/News.js
+++ src/Components/News.js
@@ -7,29 +7,59 @@ export class News extends Component {
         super();
         this.state = {
             articles: this.articles,
-            loading: false
+            loading: false,
+            page: 1,
+            totalResults: this.totalResults
         }
     }
 
     async componentDidMount() {
-        let url = "https://newsapi.org/v2/top-headlines?country=us&apiKey=c37e2ef951bf4f0caf7edba576d573eb"
+        let url = "https://newsapi.org/v2/everything?domains=techcrunch.com,thenextweb.com&apiKey=c37e2ef951bf4f0caf7edba576d573eb"
         let data = await fetch(url);
         let parsedData = await data.json();
         this.setState({ articles: parsedData.articles })
+
+    }
+
+    nextBtnHandler = async () => {
+        if (this.state.page + 1 > Math.ceil(this.state.totalResults / 100)) {
+        }
+        else {
+            let url = `https://newsapi.org/v2/everything?domains=techcrunch.com,thenextweb.com&apiKey=c37e2ef951bf4f0caf7edba576d573eb&page=${this.state.page + 1}`;
+            let data = await fetch(url);
+            let parsedData = await data.json();
+            this.setState({
+                page: this.state.page + 1,
+                articles: parsedData.articles
+            })
+        }
+    }
+    previousBtnHandler = async () => {
+        let url = `https://newsapi.org/v2/everything?domains=techcrunch.com,thenextweb.com&apiKey=c37e2ef951bf4f0caf7edba576d573eb&page=${this.state.page - 1}`;
+        let data = await fetch(url);
+        let parsedData = await data.json();
+        this.setState({
+            page: this.state.page - 1,
+            articles: parsedData.articles
+        })
+
     }
     render() {
         return (
-            <div className="container my-3">
+            <div className="container my-3 pd-4">
                 <h2>NewsMonkey - Top Headlines</h2>
-                <div className="row">
+                <div className="row rounded-4" style={{ backgroundColor: '#e5e5e5' }} >
                     <div className="row">
                         {this.state.articles.map((element) => {
-                            return <div className="col-md-4" key={element.url}>
-                                <NewsItems title={element.title} description={element.description} imageUrl={element.urlToImage} newsUrl={element.url} />
+                            return <div className="col" key={element.url}>
+                                <NewsItems title={element.title.slice(0, 64)} description={element.description.slice(0, 88)} imageUrl={element.urlToImage} newsUrl={element.url} />
                             </div>
                         })}
                     </div>
-
+                    <div className="container d-flex justify-content-between">
+                        <button type="button" disabled={this.state.page <= 1} className="btn btn-success" onClick={this.previousBtnHandler}>&larr; Previous</button>
+                        <button type="button" className="btn btn-success" onClick={this.nextBtnHandler}>Next &rarr;</button>
+                    </div>
                 </div>
             </div>
         )
--- src/Components/NewsItems.js
+++ src/Components/NewsItems.js
@@ -3,15 +3,15 @@ import React, { Component } from 'react'
 export class NewsItems extends Component {
     render() {
 
-        let { title, discripion, imageUrl, newsUrl } = this.props;
+        let { title, description, imageUrl, newsUrl } = this.props;
         return (
-            <div className="conatainer">
-                <div>
-                    <div className="card" style={{ width: "18rem" }}>
-                        <img src={imageUrl} className="card-img-top" alt="..." />
-                        <div className="card-body">
-                            <h5 className="card-title">{title}</h5>
-                            <p className="card-text">{discripion}</p>
+            <div className="conatainer my-3 " >
+                <div className="align-items-center">
+                    <div className="card" style={{ width: "18rem", minHeight: "389.91px" }}>
+                        <img src={!(imageUrl) ? "https://media.istockphoto.com/id/1264074047/vector/breaking-news-background.jpg?s=612x612&w=0&k=20&c=C5BryvaM-X1IiQtdyswR3HskyIZCqvNRojrCRLoTN0Q=" : imageUrl} className="card-img-top" alt="..." style={{ height: "149.53px" }} />
+                        <div className="card-body ">
+                            <h5 className="card-title">{title}...</h5>
+                            <p className="card-text">{description}...</p>
                             <a href={newsUrl} target="_blank" rel='noreferrer' className="btn btn-primary">Read More...</a>
                         </div>
                     </div>
